d0133950d8ee39fac81b5f5e6345d511d84de3b5
Camden Narzt <c.narzt@me.com>
2022-12-31 03:59:43 -0700

    add more tests

--- Tests/CgetargvTests/Test.swift
+++ Tests/CgetargvTests/Test.swift
@@ -7,8 +7,32 @@ final class CgetargvTests: XCTestCase {
         let options = GetArgvOptions(skip: 0, pid: getpid(), nuls: true)
         var res = ArgvResult();
         XCTAssert(withUnsafePointer(to: options, { get_argv_of_pid($0, &res) }))
+
         let expectedOutput = ProcessInfo.processInfo.arguments.joined(separator: " ")
         let actualOutput = String(cString: res.start_pointer)
+
+        XCTAssertEqual(actualOutput, expectedOutput, "The Args are not correct.")
+    }
+
+    func testGetArgvOfPidWithNuls() {
+        let options = GetArgvOptions(skip: 0, pid: getpid(), nuls: false)
+        var res = ArgvResult();
+        XCTAssert(withUnsafePointer(to: options, { get_argv_of_pid($0, &res) }))
+
+        let expectedOutput = ProcessInfo.processInfo.arguments.flatMap { $0.utf8CString }
+        let actualOutput = Array(UnsafeBufferPointer<CChar>(start: res.start_pointer, count: res.end_pointer - res.start_pointer))
+
+        XCTAssertEqual(actualOutput, expectedOutput, "The Args are not correct.")
+    }
+
+    func testGetArgvAndArgcOfPid() {
+        let pid = getpid()
+        var res = ArgvResult();
+        XCTAssert(get_argv_and_argc_of_pid(pid, &res))
+
+        let expectedOutput = ProcessInfo.processInfo.arguments
+        let actualOutput = Array(UnsafeBufferPointer<UnsafeMutablePointer<CChar>>(start: res.argv, count: res.argc)).map { String(cString: $0) }
+
         XCTAssertEqual(actualOutput, expectedOutput, "The Args are not correct.")
     }
 }
