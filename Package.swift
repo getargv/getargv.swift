9c0d0be9021ae2baa47a7e6558b30f08a4341132
Camden Narzt <c.narzt@me.com>
2023-03-14 15:25:36 -0600

    bump dep

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .library(name: "Cgetargv", targets: ["Cgetargv"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0")
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.2")
   ],
   targets: [
     .executableTarget(name: "getargv2", dependencies: [ "SwiftGetargv", .product(
