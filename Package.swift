67131f22c9b246e877191ed3c3b683aed35fe124
Camden Narzt <c.narzt@me.com>
2025-11-09 12:09:32 -0700

    Add new test

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Getargv",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v13)],
     products: [
         .library(name: "SwiftGetargv", targets: ["SwiftGetargv"]),
         .library(name: "Cgetargv",     targets: ["Cgetargv"])
--- Tests/SwiftGetargvTests/Test.swift
+++ Tests/SwiftGetargvTests/Test.swift
@@ -1,6 +1,39 @@
 import Foundation
 import Testing
 
+public extension Optional {
+    /// Boolean indicating that the Optional contains a value
+    var isSome: Bool {
+        return !self.isNone
+    }
+    /// Boolean indicating that the Optional does not contain a value
+    var isNone: Bool {
+        switch self {
+        case .none:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+public extension Result {
+    /// Boolean indicating that the Result contains a success value
+    var isSuccess: Bool {
+        return !self.isFailure
+    }
+    /// Boolean indicating that the Result contains a failure value
+    var isFailure: Bool {
+        switch self {
+        case .failure:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+
 @testable import SwiftGetargv
 
 @Suite()
@@ -24,5 +57,14 @@ struct SwiftGetargvTests {
             let actualOutput = try #require(try getArgvAndArgcOfPid(pid: getpid(), encoding: String.Encoding.nonLossyASCII).get())
 
             #expect(actualOutput == expectedOutput)
+        }
+
+    @Test("test invalid pid returns a failure")
+        func invalidPid() {
+            let res = getArgvOfPid(pid: -1)
+            #expect(res.isFailure)
+            if case let .failure(errno) = res {
+                #expect(errno == .noSuchProcess)
+            }
         }
 }
